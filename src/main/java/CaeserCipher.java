e473462c63101ea7b47a4c2d4627e7625ecc444c
MKaringa <mungaikaringa1@gmail.com>
2021-06-01 16:16:57 +0300

    Create a constrouctor that takes message and key

--- src/main/java/CaeserCipher.java
+++ src/main/java/CaeserCipher.java
@@ -2,4 +2,9 @@ public class CaeserCipher {
 
     private String theMessage;
     private int shiftKey;
+
+    public CaeserCipher(String message, int key){
+        this.theMessage = message;
+        this.shiftKey = key;
+    }
 }

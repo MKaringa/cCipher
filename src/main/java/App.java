a4254ba145ab7008006697b7e6b172eb0680f379
MKaringa <mungaikaringa1@gmail.com>
2021-06-02 10:17:38 +0300

    Gather inputs from the user, add a boolean that tells the program when to stop

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-    }
-}
+
+        boolean cipherActive = true;
+
+        while (cipherActive) {
+            System.out.println("Please type yes if you want to continue else type no to exit");
+            String response = scan.nextLine();
+            if (response.equals("yes")) {
+                System.out.println("Enter the message to encrypt.");
+                String message = scan.nextLine();
+                System.out.println("Enter the key to encrypt the message");
+                int key = scan.nextInt();
+
+                CaeserCipher caeserCipher = new CaeserCipher(message, key);
+                String encryptedMessage = caeserCipher.messageEncryption();
+                String decryptedMessage = caeserCipher.messageDecryption();
+
+                System.out.println("Your message encrypted is : " + encryptedMessage);
+                System.out.println("Your message decrypted is : " + decryptedMessage);
+
+            }
+        }

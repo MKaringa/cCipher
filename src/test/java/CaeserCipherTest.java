865fd8d28346c2c7d55308004d9c4a25c87ff0b1
MKaringa <mungaikaringa1@gmail.com>
2021-06-02 08:49:51 +0300

    Test whether the get method works.

--- src/test/java/CaeserCipherTest.java
+++ src/test/java/CaeserCipherTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CaeserCipherTest {
+    @Test
+    public void newCaesarCipher_getMessage_running() {
+        CaeserCipher testCaesarCipher = new CaeserCipher("running",5);
+        assertEquals("hi",testCaesarCipher.getTheMessage());
+    }
 
 }
